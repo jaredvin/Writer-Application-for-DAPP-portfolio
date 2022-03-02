75bd3db4d394a8da3275d750325d693f99cb6115
jaredvin <98550767+jaredvin@users.noreply.github.com>
2022-03-01 19:09:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var placeholder = document.getElementById('survey_options');
+var add_more_fields = document.getElementById('add_more_fields');
+var remove_fields = documnet.getElementById('remove_fields');
+
+add_more_fields.onclick = function(){
+    var newfield = document.createElement('input');
+    
+
+}   
